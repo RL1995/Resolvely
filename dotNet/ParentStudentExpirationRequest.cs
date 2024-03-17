e66f6a6944da30e56362702f6773fdc3c491df07
RL1995 <119047856+RL1995@users.noreply.github.com>
2024-03-17 15:55:10 -0400

    Add files via upload

---
+++ dotNet/ParentStudentExpirationRequest.cs
@@ -0,0 +1,17 @@
+﻿using Sabio.Models.Domain.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Domain.ParentStudent
+{
+    public class ParentStudentExpirationRequest : BaseUser
+    {
+
+        public  DateTime ExpirationDate { get; set; }
+        public bool IsConfirmed {  get; set; }
+
+    }
+}
